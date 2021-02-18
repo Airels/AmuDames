1fd13edc124803111301ba62298886c59cd626b0
Airels <yohan13240@hotmail.fr>
2021-02-18 10:43:00 +0100

    ajout de modeles pour user et news

---
+++ server/server/models/news.models.js
@@ -0,0 +1,8 @@
+class News {
+    constructor(title, type, date, content) {
+        this.title = title;
+        this.type = type;
+        this.date = date;
+        this.content = content;
+    }
+}
