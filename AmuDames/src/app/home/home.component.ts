01ffbd60338bdebbb09c4126de74b7bd0010679a
m-o-k-a <eddy.ikhlef@protonmail.com>
2021-02-18 10:47:36 +0100

    mise en place du getter de news

--- AmuDames/src/app/home/home.component.ts
+++ AmuDames/src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { News } from '../models/news.models';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  newsList!: News[];
+  newsSubscription!: Subscription;
 
-  constructor() { }
+  constructor(private http: HttpService) { 
+    this.newsSubscription = this.http.getNews(10).subscribe(
+      (newsList: News[]) => { this.newsList = newsList; }
+    );
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.newsSubscription.unsubscribe();
   }
 
 }
