7ca3b2627055fb080135d480cb4773e079a68101
m-o-k-a <eddy.ikhlef@protonmail.com>
2021-02-18 09:59:03 +0100

    squelette du routage

--- AmuDames/src/app/app-routing.module.ts
+++ AmuDames/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component:  HomeComponent },
+  { path: 'play', component:  PlayComponent },
+  { path: 'ranking', component: RankingComponent },
+  { path: 'guide', component: GuideComponent },
+  { path: 'not-found', component: 'home' },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
