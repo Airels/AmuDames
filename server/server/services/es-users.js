767973f00f6a06f71768489b47397000c4ee878f
Hyo <kevinp966@gmail.com>
2021-02-25 03:53:55 +0100

    Ajout dans es-users.js de certaines méthodes à tester (login et register car route absente) d'autres à compléter sur le getUser pour qu'il return toutes les infos

--- server/server/services/es-users.js
+++ server/server/services/es-users.js
@@ -10,22 +10,76 @@ const handleElasticsearchError = (error) => {
     throw new Error(error.msg, error.status || 500);
 }
 
-const addUser = async (username, password, email, elo, country, profileImageURL, descrption) => { // Retourne 201 si ok
-    return 501;
+const addUser = async (username, password, email, elo, country, profileImageURL, descrption) => { 
+    es.index({
+        index:'users',
+        type: 'user',
+        body:{
+            'username':username,
+            'password':password,
+            'email':email,
+            'elo':elo,
+            'country':country,
+            'profileImg':profileImageURL,
+            'description':descrption,
+            'isAdmin':'false'
+        }
+    }).then(response => response).catch((error) => {
+        handleElasticsearchError(error);
+    });
 };
+
 
 const userLogin = async (email, password) => { // Retourne 401 si non trouvé / erreur d'auth, sinon retourner 200
-    return 501;
+        es.search({
+            index:'users',
+            type:'user',
+            body:{
+                query:{
+                    match: { 'email' : email, 'password':password },
+                }
+            }
+        }).then(response => response).catch((error) => {
+            handleElasticsearchError(error);
+        });
 };
 
-const getUser = { // Retour 404 si non trouvé, sinon TOUTES les infos
+const getUser = { // Retour 404 si non trouvé, à faire return de toutes les infos
     byId: async (id) => {
+        es.search({
+            index:'users',
+            type:'user',
+            body:{
+                query:{
+                    match: { 'id' : email },
+                }
+            }
+        })
+
         return 501;
     },
     byUsername: async (username) => {
+        es.search({
+            index:'users',
+            type:'user',
+            body:{
+                query:{
+                    match: { 'username' : username },
+                }
+            }
+        })// à faire return de toutes les infos
         return 501;
     },
     byEmail: async (email) => {
+        es.search({
+            index:'users',
+            type:'user',
+            body:{
+                query:{
+                    match: { 'email' : email },
+                }
+            }
+        })// à faire return de toutes les infos
         return 501;
     }
 };
@@ -36,7 +90,17 @@ const updateUser = async (username) => {
 
 // Retour 200 si ok
 const deleteUser = async (username) => {
-    return 501;
+    es.deleteByQuery({
+        index:'users',
+        type:'user',
+        body:{
+            query:{
+                match: { 'username' : username },
+            }
+        }
+    }).then(response => response).catch((error) => {
+        handleElasticsearchError(error);
+    });
 };
 
 export default {
