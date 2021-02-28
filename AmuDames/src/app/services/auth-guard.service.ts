925ccc341dd3d61a78a4cc38d8e0126a2a2cbd7b
m-o-k-a <eddy.ikhlef@protonmail.com>
2021-02-28 14:26:25 +0100

    auth

--- AmuDames/src/app/services/auth-guard.service.ts
+++ AmuDames/src/app/services/auth-guard.service.ts
@@ -1,22 +1,26 @@
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { UserService } from './user.service';
+import { User } from '../models/user.models';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+    user!: User | null;
 
-    constructor(public authService: AuthService, private router: Router) {
-        
+    constructor(public userService: UserService, private router: Router) {
+        this.userService.userSubject.subscribe((user) => {
+            this.user = user;
+          });
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        if(this.authService.isAuth === true) {
-            //alert('GUARD: connected');
+        if(this.user != null && this.user != undefined) {
+            alert('GUARD: connected');
             return true;
         } else {
-            //alert('GUARD: not connected');
+            alert('GUARD: not connected');
             //this.router.navigate(['home']);
             return false;
         }
