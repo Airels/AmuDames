4e035bac9b28bed43aa334b0bdb28469b25212db
m-o-k-a <eddy.ikhlef@protonmail.com>
2021-03-05 19:40:34 +0100

    begin to work on user profile

--- AmuDames/src/app/user-profile/user-profile.component.ts
+++ AmuDames/src/app/user-profile/user-profile.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable, Subscription, from } from 'rxjs';
 import { User } from '../models/user.models';
 import { HttpService } from '../services/http.service';
 import {UserService} from '../services/user.service';
+import { validatePassword, validateCountry } from '../customValidators';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,34 +13,46 @@ import {UserService} from '../services/user.service';
 })
 export class UserProfileComponent implements OnInit {
   userObservable !: Observable<User>;
-  user!: User |null;
+  activeUser !: User;
+  viewUser !: User;
+  editForm!: FormGroup;
+  isCollapsed = true;
 
-  constructor(private http : HttpService, public userService : UserService, public userSubscription : User) { 
+  constructor(private formBuilder: FormBuilder, private http : HttpService, public userService : UserService, public userSubscription : User) { 
     
   }
 
   ngOnInit(): void {
+    this.editForm = this.formBuilder.group({
+      username: [this.viewUser.username, [Validators.required]],
+      email: [this.viewUser.email, [Validators.required, Validators.email]],
+      country: [this.viewUser.country, [Validators.required]],
+      profilePicture: [this.viewUser.profileImageURL, [Validators.required]],
+      description: [this.viewUser.description, [Validators.required, Validators.maxLength(25)]],
+      password: ['', []],
+      passwordConfirm: ['', []],
+      options: this.formBuilder.array([])
+    }, { validator: [validatePassword, validateCountry] });
+
     this.userObservable = this.http.getCurrentUser();
 
     this.userService.userSubject.subscribe((user) => {
-      this.user = user;
+      this.activeUser = user;
     });
-      
-    
   }
-  
 
+  onSubmitEditUser(): void {
+    var formValue = this.editForm.value;
+    const updateUser = new User(
+      formValue['username'],
+      formValue['password'],
+      formValue['email'],
+      this.viewUser.elo,
+      formValue['profilePicture'],
+      formValue['country'],
+      formValue['description'],
+      this.viewUser.isAdmin
+    );
+    this.http.updateUser(updateUser);
+  }
 }
-
-/*
-  ngOnInit() {
-    //this.students = this.StudentService.students;
-    this.isAuth = this.authService.isAuth;
-    this.studentSubscription = this.StudentService.studentsSubject.subscribe(
-      (students: any[]) => {
-      this.students = students;
-      }
-      );
-      this.StudentService.emitStudentSubject();
-  }
-  */
