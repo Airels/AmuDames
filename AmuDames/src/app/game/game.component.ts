f77d969168e7c2f5ee515e48fa457ef633875858
m-o-k-a <eddy.ikhlef@protonmail.com>
2021-03-06 19:05:02 +0100

    tried again

--- AmuDames/src/app/game/game.component.ts
+++ AmuDames/src/app/game/game.component.ts
@@ -120,13 +120,13 @@ export class GameComponent implements OnInit, OnDestroy {
           //convert from computer value to math value (change start 0 to start 1)
           if(this.selected?.length == 2 && this.selected[0] != pos[0] && this.selected[1] != pos[1] && this.isPlaying) {
             if(this.isWhite) {
-              this.selected[0] += 1;  this.selected[1] += 1;
+              this.selected[0] = 1;  this.selected[1] += 1;
               pos[0] += 1;  pos[1] += 1;
-              this.gameService.movePawn(this.selected, pos);
+              this.gameService.movePawn(this.selected.reverse, pos.reverse);
             } else {
               this.selected[0] = (9-this.selected[0])+1;  this.selected[1] = (9-this.selected[1])+1;
               pos[0] = (9-pos[0])+1;  pos[1] = (9-pos[1])+1;
-              this.gameService.movePawn(this.selected, pos);
+              this.gameService.movePawn(this.selected.reverse, pos.reverse);
             }
           }
         }
@@ -171,7 +171,7 @@ export class GameComponent implements OnInit, OnDestroy {
       for (var i = 0; i<this.nRow; i++) {
         for (var j = 0; j<this.nRow; j++) {
           if((this.x > j*w && this.x < (j+1)*w) && (this.y > i*w && this.y < (i+1)*w)) {
-            return [j, i];
+            return [i, j];
           }
         }
       }
