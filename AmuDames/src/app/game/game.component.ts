389d8d4c7f71dea9710276c70f8f174d0cc162b4
m-o-k-a <eddy.ikhlef@protonmail.com>
2021-03-06 19:29:02 +0100

    removed incrementation, we start from zero on serv

--- AmuDames/src/app/game/game.component.ts
+++ AmuDames/src/app/game/game.component.ts
@@ -120,12 +120,10 @@ export class GameComponent implements OnInit, OnDestroy {
           //convert from computer value to math value (change start 0 to start 1)
           if(this.selected?.length == 2 && this.selected[0] != pos[0] && this.selected[1] != pos[1] && this.isPlaying) {
             if(!this.isWhite) {
-              this.selected[0] = (this.selected[0]+1)%9;   this.selected[1] = (this.selected[1]+1)%9;
-              pos[0] = (pos[0]+1)%9;  pos[1] = (pos[1]+1)%9;
               this.gameService.movePawn(this.selected, pos);
             } else {
-              this.selected[0] = ((9-this.selected[0])+1)%9;  this.selected[1] = ((9-this.selected[1])+1)%9;
-              pos[0] = ((9-pos[0])+1)%9;  pos[1] = ((9-pos[1])+1)%9;
+              this.selected[0] = (9-this.selected[0]);  this.selected[1] = (9-this.selected[1]);
+              pos[0] = (9-pos[0]);  pos[1] = (9-pos[1]);
               this.gameService.movePawn(this.selected, pos);
             }
           }
